191f3b0e892283e0d08e263dd9f357f64589447d
songhashin <shsha9292@gmail.com>
2026-05-11 15:21:38 +0900

    chore: clean up packages

--- src/main/kotlin/me/helloworld/sample/common/PersistenceConfig.kt
+++ src/main/kotlin/me/helloworld/sample/common/PersistenceConfig.kt
@@ -1,12 +1,12 @@
-package me.helloworld.common
+package me.helloworld.sample.common
 
-import javax.sql.DataSource
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import javax.sql.DataSource
 
 @Configuration
 @EnableJpaRepositories(basePackages = ["me.helloworld.mvc"])
